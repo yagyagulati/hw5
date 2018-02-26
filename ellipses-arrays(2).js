fbe811f6b9760479b95a1bbf5091d0d3d65c3114
yagyagulati <yagyagulati@Yagyas-MacBook-Pro.local>
2018-02-26 10:17:11 -0800

    hw

--- ellipses-arrays(2).js
+++ ellipses-arrays(2).js
@@ -2,18 +2,23 @@ let drops = [];
 
 function setup() {
   createCanvas(400, 400);
-	 for (let i = 0; i < 100; i++) {
+	 for (let i = 0; i < 50; i++) {
    let x=10+100*i;
 		 drops[i] = new drop(x,200,20);
 	 }
 }
   
 function draw() {
-	background(255,255,0);
+	background(mouseX,random(255),mouseY);
 for (let i=0;i<drops.length;i++){
 	drops[i].show();
 	drops[i].move();
 }
+	stroke(0);
+	fill(0);
+	strokeWeight(1);
+
+		text('Inspiration from "The Coding Train"', 50,380);
 }
 
 class drop{
@@ -31,6 +36,10 @@ class drop{
 fill(0,30);
 stroke(255,255,0);
 		strokeWeight(4);
-		ellipse(this.x,this.y, this.r*2);
+		ellipse(mouseX,this.y, this.r*2);
+		ellipse(mouseX+100,this.y, this.r*2);
+		ellipse(this.x,mouseY, this.r*2);
+		ellipse(this.x,mouseY+100, this.r*2);
+
 	}
 }
